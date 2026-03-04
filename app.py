8b837ad3d1b2956931ecacd5c5aea9be5996a713
monikak2004 <142688129+monikak2004@users.noreply.github.com>
2026-03-04 19:07:29 +0530

    fix run block

--- app.py
+++ app.py
@@ -812,19 +812,5 @@ def get_order_items(order_id):
 # ==============================
 # RUN
 # ==============================
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
